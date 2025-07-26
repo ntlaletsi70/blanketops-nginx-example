727eb23f7d6bd9e4ca8d7306025a1f3f04e5fb00
Neo Tlaletsi <ntlaletsi86@gmail.com>
2025-07-26 23:06:24 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_, res) => res.send("Hello from BlanketOps!"));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
