1c761648e927bef2cd166dceea773810aa11ba52
Neo Tlaletsi <ntlaletsi86@gmail.com>
2025-07-26 23:22:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-index.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_, res) => res.send("Hello from BlanketOps!"));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
